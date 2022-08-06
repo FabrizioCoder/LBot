51df5a4c210832cfa96ce1d1d81631fb00062e46
FabrizioCoder <fabrizioivan36@gmail.com>
2022-08-06 09:16:09 -0500

    New Command: HelpCommand

---
+++ src/main/java/me/fabriziocoder/luxanna/commands/core/HelpCommand.java
@@ -0,0 +1,42 @@
+package me.fabriziocoder.luxanna.commands.core;
+
+import com.jagrosh.jdautilities.command.SlashCommand;
+import com.jagrosh.jdautilities.command.SlashCommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.util.Arrays;
+
+public class HelpCommand extends SlashCommand {
+
+    public static final String COMMAND_NAME = "help";
+    public static final String COMMAND_DESCRIPTION = "Shows help options for all commands";
+
+    @Override
+    public String getName() {
+        return COMMAND_NAME;
+    }
+
+    @Override
+    public String getHelp() {
+        return COMMAND_DESCRIPTION;
+    }
+
+    @Override
+    public void execute(SlashCommandEvent event) {
+        StringBuilder leagueCommands = new StringBuilder();
+        StringBuilder coreCommands = new StringBuilder();
+        event.deferReply().queue();
+        event.getClient().getSlashCommands().forEach(command -> {
+            if (command.getName().equals("lol")) {
+                Arrays.stream(command.getChildren()).map(SlashCommand::getName).forEach((it) -> leagueCommands.append("`").append(it).append("` "));
+            } else {
+                coreCommands.append("`").append(command.getName()).append("` ");
+            }
+        });
+
+        MessageEmbed messageEmbed = new EmbedBuilder().setColor(0x2564f4).setTitle(String.format("%s — Commands", event.getJDA().getSelfUser().getName())).setDescription(String.format("Hi! I'm %s, the perfect way to play your games.", event.getJDA().getSelfUser().getName())).addField("» League of Legends", String.join("", leagueCommands), false).addField("» Core", String.join("", coreCommands), false).setThumbnail(event.getJDA().getSelfUser().getAvatarUrl()).build();
+
+        event.getHook().sendMessageEmbeds(messageEmbed).queue();
+    }
+}
