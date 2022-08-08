345a587c5346985685670eb07a20b809f5876f0b
FabrizioCoder <fabrizioivan36@gmail.com>
2022-08-08 14:46:41 -0500

    Update Command CurrentSubCommand: Unix timestamp

--- src/main/java/me/fabriziocoder/luxanna/commands/league/summoner/CurrentSubCommand.java
+++ src/main/java/me/fabriziocoder/luxanna/commands/league/summoner/CurrentSubCommand.java
@@ -14,8 +14,6 @@ import no.stelar7.api.r4j.basic.constants.types.lol.TeamType;
 import no.stelar7.api.r4j.pojo.lol.spectator.SpectatorGameInfo;
 import no.stelar7.api.r4j.pojo.lol.summoner.Summoner;
 
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -78,7 +76,7 @@ public class CurrentSubCommand extends SlashCommand {
 
         EmbedBuilder messageEmbed = new EmbedBuilder().setColor(0x2564f4).setThumbnail(SummonerUtils.makeProfileIconURL(String.valueOf(summonerData.getProfileIconId()))).setAuthor("Current match").setTitle(String.format("`%s` - `%s`", summonerData.getName(), summonerData.getPlatform().prettyName()));
 
-        String[] gameInformation = {String.format("`Map:` %s", summonerCurrentGame.getMap().prettyName()), String.format("`Game mode:` %s", summonerCurrentGame.getGameMode().prettyName()), String.format("`Game type:` %s", summonerCurrentGame.getGameType().prettyName()), String.format("`Queue type:` %s", summonerCurrentGame.getGameQueueConfig().prettyName()), String.format("`Game start:` %s", DateTimeFormatter.ofPattern("dd/MM/yyyy - hh:mm:ss").withZone(ZoneId.systemDefault()).format(summonerCurrentGame.getGameStartAsDate())),};
+        String[] gameInformation = {String.format("`Map:` %s", summonerCurrentGame.getMap().prettyName()), String.format("`Game mode:` %s", summonerCurrentGame.getGameMode().prettyName()), String.format("`Game type:` %s", summonerCurrentGame.getGameType().prettyName()), String.format("`Queue type:` %s", summonerCurrentGame.getGameQueueConfig().prettyName()), String.format("`Game start:` <t:%s:R>", summonerCurrentGame.getGameStart() / 1000)};
         messageEmbed.addField("> Match infomation", String.join("\n", gameInformation), false);
 
         if (!summonerCurrentGame.getBannedChampions().isEmpty()) {
